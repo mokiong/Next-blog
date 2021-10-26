bb126f9f746b177dce289e3566896c39593db386
Moki <mokiong1427@gmail.com>
2021-10-26 16:54:08 +0800

    Get data from static props

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,18 @@
 import AllPosts from '../../components/posts/Allposts';
+import { getAllPosts } from '../../lib/postsUtil';
 
-function AllPostPage() {
-    return <AllPosts posts={DUMMY_POSTS} />;
+function AllPostPage(props) {
+    return <AllPosts posts={props.posts} />;
+}
+
+export function getStaticProps() {
+    const allPosts = getAllPosts();
+
+    return {
+        props: {
+            posts: allPosts,
+        },
+    };
 }
 
 export default AllPostPage;
