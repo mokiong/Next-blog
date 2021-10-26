18b2001464867f3778c1749830dd89dd3b93e619
Moki <mokiong1427@gmail.com>
2021-10-26 18:39:00 +0800

    Store messages on database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
 
@@ -19,7 +21,30 @@ function handler(req, res) {
             message,
         };
 
-        console.log(newMessage);
+        let client;
+
+        try {
+            client = await MongoClient.connect(
+                'mongodb+srv://testAdmin:ZNVbPfYH2qksUayF@cluster0.an94b.mongodb.net/next-blog?retryWrites=true&w=majority'
+            );
+        } catch (err) {
+            return res.status(500).json({ message: err.message });
+        }
+
+        try {
+            const db = client.db();
+            const result = await db
+                .collection('messages')
+                .insertOne(newMessage);
+
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            return res.status(500).json({ message: error.message });
+        }
+
+        client.close();
+
         return res
             .status(201)
             .json({ message: 'Success', message: newMessage });
