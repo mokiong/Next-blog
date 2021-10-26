a8e58e1d6cfa3f74235910efc48a1a23c01351a1
Moki <mokiong1427@gmail.com>
2021-10-26 21:34:15 +0800

    Add heading data

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,16 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
 import ContactForm from '../components/contact/ContactFrom';
 
 function ContactPage() {
-    return <ContactForm />;
+    return (
+        <Fragment>
+            <title>Contact Me</title>
+            <meta name="description" content="Send me your messages" />
+            <ContactForm />
+        </Fragment>
+    );
 }
 
 export default ContactPage;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import Head from 'next/head';
 
 import FeaturedPosts from '../components/home-page/FeaturedPost';
 import Hero from '../components/home-page/Hero';
@@ -7,6 +8,13 @@ import { getFeaturedPosts } from '../lib/postsUtil';
 function HomePage(props) {
     return (
         <Fragment>
+            <Head>
+                <title>Max's blog</title>
+                <meta
+                    name="description"
+                    content="I post about programming and web development"
+                />
+            </Head>
             <Hero />
             <FeaturedPosts posts={props.posts} />
         </Fragment>
