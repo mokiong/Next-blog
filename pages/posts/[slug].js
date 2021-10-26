6c03620b38afae5c8a958bd89053ccb45d64aa35
Moki <mokiong1427@gmail.com>
2021-10-26 17:08:32 +0800

    Get post from markdown file

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,35 @@
 import PostContent from '../../components/posts/post-detail/PostContent';
+import { getPostData, getPostFiles } from '../../lib/postsUtil';
 
-function PostDetailPage() {
-    return <PostContent />;
+function PostDetailPage(props) {
+    return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+    const { params } = context;
+    const { slug } = params;
+
+    const postData = getPostData(slug);
+
+    return {
+        props: {
+            post: postData,
+        },
+        revalidate: 600,
+    };
+}
+
+export function getStaticPaths() {
+    const postFileNames = getPostFiles();
+
+    const slugs = postFileNames.map((filename) =>
+        filename.replace(/\.md$/, '')
+    );
+
+    return {
+        paths: slugs.map((slug) => ({ params: { slug: slug } })),
+        fallback: false,
+    };
 }
 
 export default PostDetailPage;
